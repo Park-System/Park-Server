2da5bfb5cfa060ad1555927a8bf484c0caca5653
kcsc2217 <k12002@nate.com>
2024-10-29 10:53:55 +0900

    feat :swagger 세팅

---
+++ src/main/java/parkSystem/park/common/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package parkSystem.park.common.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("주차 api") // API의 제목
+                .description("Parking Service api") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
+    }
+}
