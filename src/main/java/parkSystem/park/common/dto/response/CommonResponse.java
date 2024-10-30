f194cd8d36a542abd88f4c17392932a7c4fd4510
akk <rudrhs9309@naver.com>
2024-10-31 00:33:17 +0900

    feat: 공통 응답 DTO 추가

---
+++ src/main/java/parkSystem/park/common/dto/response/CommonResponse.java
@@ -0,0 +1,11 @@
+package parkSystem.park.common.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommonResponse {
+    private String code;
+    private String message;
+}
--- src/main/java/parkSystem/park/coupon/controller/CouponController.java
+++ src/main/java/parkSystem/park/coupon/controller/CouponController.java
@@ -1,9 +1,12 @@
 package parkSystem.park.coupon.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import parkSystem.park.common.dto.response.CommonResponse;
 import parkSystem.park.coupon.dto.request.CouponReqDTO;
 import parkSystem.park.coupon.service.CouponService;
 
@@ -13,8 +16,10 @@ public class CouponController {
 
     private final CouponService couponService;
 
-    @PostMapping("")
-    public void createCoupon(@RequestBody CouponReqDTO couponReqDTO){
+    @PostMapping("/coupon/register")
+    public ResponseEntity<CommonResponse> createCoupon(@RequestBody CouponReqDTO couponReqDTO){
         couponService.createCoupon(couponReqDTO);
+        CommonResponse commonResponse = new CommonResponse("200 OK", "쿠폰이 등록되었습니다.");
+        return new ResponseEntity<>(commonResponse, HttpStatus.OK);
     }
 }
