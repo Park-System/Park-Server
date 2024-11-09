5348d832b3610f089bb0abaa8afd7bf0aaa6c95b
ensalada-de-pechuga <villop020403@o.cnu.ac.kr>
2024-11-09 21:59:20 +0900

    park domain 추가

---
+++ src/main/java/parkSystem/park/park/domain/ParkingSpot.java
@@ -0,0 +1,37 @@
+package parkSystem.park.park.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import parkSystem.park.park.domain.enums.ParkingType;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ParkingSpot {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "parking_spot_id")
+  private Long id;
+
+
+  @Enumerated(EnumType.STRING)
+  private ParkingType parkingType;
+
+  private String parkingSpotName;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parking_info_id")
+  private ParkingInfo parkingInfo;
+
+
+  @Builder
+  public ParkingSpot(ParkingType parkingType, String parkingSpotName, ParkingInfo parkingInfo) {
+    this.parkingType = parkingType;
+    this.parkingSpotName = parkingSpotName;
+    this.parkingInfo = parkingInfo;
+  }
+}
---
+++ src/main/java/parkSystem/park/park/domain/enums/ParkingType.java
@@ -0,0 +1,8 @@
+package parkSystem.park.park.domain.enums;
+
+public enum ParkingType {
+
+  PREGNANT,
+  DISABLED,
+  NORMAL
+}
