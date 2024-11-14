e2f28d7e54528ab6636d16b7948a97ff097de1e2
kcsc2217 <k12002@nate.com>
2024-11-14 12:53:41 +0900

    feat : swagger spring docs 의존성 ㅊ추가

--- src/main/java/parkSystem/park/common/config/SwaggerConfig.java
+++ src/main/java/parkSystem/park/common/config/SwaggerConfig.java
@@ -3,17 +3,34 @@ package parkSystem.park.common.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(
+                        new Components()
+                                // accessToken이라는 스키마 만들어주기
+                                .addSecuritySchemes("accessToken", new SecurityScheme()
+                                        .name("access")
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .bearerFormat("JWT")
+                                )
+                                // refreshToken이라는 스키마 만들어주기
+                                .addSecuritySchemes("refreshToken", new SecurityScheme()
+                                        .name("refreshToken")
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .bearerFormat("JWT")
+                                )
+                );
     }
+
 
     private Info apiInfo() {
         return new Info()
