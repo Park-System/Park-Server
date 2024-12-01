24e28831e7c6223aa5c6d5971fbbba1b7bbcca7d
akk <rudrhs9309@naver.com>
2024-12-01 23:58:50 +0900

    feat : Redis MultiDataBase 사용을 위한 RedisConfig 수정

--- src/main/java/parkSystem/park/common/config/RedisConfig.java
+++ src/main/java/parkSystem/park/common/config/RedisConfig.java
@@ -1,36 +1,25 @@
 package parkSystem.park.common.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String host;
 
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+    @Value("${spring.redis.port}")
+    private int port;
 
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(connectionFactory);
-
-        return redisTemplate;
+    public RedisConnectionFactory createLettuceConnectionFactory(int dbIndex) {
+        final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setDatabase(dbIndex);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
-
-
-    @Bean
-    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
-
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-
-        container.setConnectionFactory(connectionFactory);
-
-        return container;
-    }
-
 }
